198787aeb26a82d91e4f69912ec1a84093412407
64J0 <vinigaio97@gmail.com>
2023-01-27 16:54:46 -0300

    docs: new page

--- src/Saturn/Utils.fs
+++
@@ -1,7 +0,0 @@
-namespace Saturn
-
-module Utils =
-
-  module String =
-    let internal equalsCaseInsensitive (a : string) (b : string) =
-      a.Equals(b, System.StringComparison.InvariantCultureIgnoreCase)
